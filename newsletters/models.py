59f5e629881aefb12f7d8ccb1b95104851a224a2
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-11 11:24:56 +0200

    Model for newsletter recipient created

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterRecipient(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email", help_text="Укажите Email получателя")
+    full_name = models.CharField(max_length=255, verbose_name="ФИО", help_text="Укажите ФИО получателя")
+    comment = models.TextField(verbose_name="Комментарий", null=True, blank=True, help_text="Укажите комментарий")
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = "Получатель рассылки"
+        verbose_name_plural = "Получатели рассылки"
