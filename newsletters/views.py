5d2819b4a71568fd1c6ea5a9bb80b4bad1df469a
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-11 18:14:15 +0200

    CRUD for NewsletterRecipient model realized

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from newsletters.forms import NewsletterRecipientForm
+from newsletters.models import NewsletterRecipient
+
+
+class NewsletterRecipientListView(ListView):
+    model = NewsletterRecipient
+    template_name = "newsletters/clients_list.html"
+    context_object_name = "clients"
+
+
+class NewsletterRecipientCreateView(CreateView):
+    model = NewsletterRecipient
+    form_class = NewsletterRecipientForm
+    template_name = "newsletters/client_form.html"
+    success_url = reverse_lazy('newsletters:clients_list')
+
+
+class NewsletterRecipientUpdateView(UpdateView):
+    model = NewsletterRecipient
+    form_class = NewsletterRecipientForm
+    template_name = "newsletters/client_form.html"
+    success_url = reverse_lazy('newsletters:clients_list')
+
+
+class NewsletterRecipientDetailView(DetailView):
+    model = NewsletterRecipient
+    template_name = 'newsletters/client_detail.html'
+    context_object_name = 'client'
+
+
+class NewsletterRecipientDeleteView(DeleteView):
+    model = NewsletterRecipient
+    template_name = 'newsletters/client_confirm_delete.html'
+    context_object_name = 'client'
+    success_url = reverse_lazy('newsletters:clients_list')
+
+# class Message(models.Model):
+#     subject = models.CharField(max_length=300, verbose_name="Тема письма", help_text="")
+#     text = models.TextField()
+#     pass
+#
+#
+#
+# class Newsletter(models.Model):
+#     pass
+#
+#
+# class NewsletterAttempt(models.Model):
+#     pass
+#
+
+
+
+
+
+
+
