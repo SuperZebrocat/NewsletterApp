7f251900ae0368bcb8f3ee0457105cc6769415ed
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-18 10:20:21 +0200

    New model Newsletter created

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class NewsletterRecipient(models.Model):
@@ -24,3 +25,33 @@ class Message(models.Model):
     class Meta:
         verbose_name = "Сообщение"
         verbose_name_plural = "Сообщения"
+
+
+class Newsletter(models.Model):
+    CREATED = "created"
+    LAUNCHED = "launched"
+    COMPLETED = "completed"
+
+    STATUS_CHOICES = [
+        (CREATED, "Рассылка создана"),
+        (LAUNCHED, "Рассылка запущена"),
+        (COMPLETED, "Рассылка завершена"),
+    ]
+    title = models.CharField(max_length=150, verbose_name="Название рассылки")
+    start_sending = models.DateTimeField(verbose_name="Дата и время начала рассылки", default=timezone.now)
+    finish_sending = models.DateTimeField(
+        verbose_name="Дата и время окончания рассылки",
+        help_text="Укажите дату и время окончания рассылки",
+        null=True,
+        blank=True,
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='created',
+        verbose_name='Статус рассылки'
+    )
+    comment = models.TextField(verbose_name="Комментарий")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='newsletters', verbose_name='Сообщение')
+    client = models.ManyToManyField(NewsletterRecipient, related_name='newsletters', verbose_name='Получатели рассылки')
