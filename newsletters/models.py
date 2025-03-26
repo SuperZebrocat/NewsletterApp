2cd717986890e704df44238c2fe387d6d04f0612
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-27 00:52:25 +0200

    Owner field for models added

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.utils import timezone
 
+from config import settings
+
 
 class NewsletterRecipient(models.Model):
     email = models.EmailField(unique=True, verbose_name="Email")
     full_name = models.CharField(max_length=255, verbose_name="ФИО")
     comment = models.TextField(verbose_name="Комментарий", null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Владелец", null=True, blank=True)
 
     def __str__(self):
-        return self.full_name
+        return self.email
 
     class Meta:
         verbose_name = "Получатель рассылки"
@@ -18,13 +21,15 @@ class NewsletterRecipient(models.Model):
 class Message(models.Model):
     subject = models.CharField(max_length=300, verbose_name="Тема письма")
     text = models.TextField(verbose_name="Текст письма")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Владелец", null=True,
+                              blank=True)
 
     def __str__(self):
         return self.subject
 
     class Meta:
-        verbose_name = "Сообщение"
-        verbose_name_plural = "Сообщения"
+        verbose_name = "Письмо"
+        verbose_name_plural = "Письма"
 
 
 class Newsletter(models.Model):
@@ -38,10 +43,9 @@ class Newsletter(models.Model):
         (COMPLETED, "Рассылка завершена"),
     ]
     title = models.CharField(max_length=150, verbose_name="Название рассылки")
-    start_sending = models.DateTimeField(verbose_name="Дата и время начала рассылки", default=timezone.now)
+    start_sending = models.DateTimeField(verbose_name="Дата и время начала рассылки", default=timezone.now, blank=True, null=True)
     finish_sending = models.DateTimeField(
         verbose_name="Дата и время окончания рассылки",
-        help_text="Укажите дату и время окончания рассылки",
         null=True,
         blank=True,
     )
@@ -52,6 +56,8 @@ class Newsletter(models.Model):
         default='created',
         verbose_name='Статус рассылки'
     )
-    comment = models.TextField(verbose_name="Комментарий")
-    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='newsletters', verbose_name='Сообщение')
-    client = models.ManyToManyField(NewsletterRecipient, related_name='newsletters', verbose_name='Получатели рассылки')
+    comment = models.TextField(verbose_name="Комментарий", blank=True, null=True)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='Письмо', related_name='newsletters')
+    clients = models.ManyToManyField(NewsletterRecipient, related_name='newsletters', verbose_name='Получатели рассылки')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Владелец", null=True,
+                              blank=True)
