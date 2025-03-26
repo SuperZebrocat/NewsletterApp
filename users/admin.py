db3337e8212cf2e0e101c9701922d69ba0277a50
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-26 22:53:08 +0200

    User model registered in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.forms import UserCreationForm
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    add_form = UserCreationForm
+    list_filter = ('id', 'email')
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # Если это создание нового пользователя
+            password = form.cleaned_data.get('password')
+            if password:
+                obj.set_password(password)  # Хэшируем пароль
+        super().save_model(request, obj, form, change)
