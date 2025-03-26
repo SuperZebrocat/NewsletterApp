e7532fe19b7718538792fd11ad47799f4c9a53ef
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-27 00:50:43 +0200

    Urls for newsletter model CRUD registered

--- newsletters/urls.py
+++ newsletters/urls.py
@@ -4,7 +4,8 @@ from newsletters.apps import NewslettersConfig
 from newsletters.views import (MessageCreateView, MessageDeleteView, MessageDetailView, MessageListView,
                                MessageUpdateView, NewsletterRecipientCreateView, NewsletterRecipientDeleteView,
                                NewsletterRecipientDetailView, NewsletterRecipientListView,
-                               NewsletterRecipientUpdateView)
+                               NewsletterRecipientUpdateView, NewsletterCreateView, NewsletterListView,
+                               NewsletterDetailView, NewsletterUpdateView, NewsletterDeleteView)
 
 app_name = NewslettersConfig.name
 
@@ -14,9 +15,16 @@ urlpatterns = [
     path("clients/<int:pk>/update/", NewsletterRecipientUpdateView.as_view(), name="client_update"),
     path("clients/<int:pk>/", NewsletterRecipientDetailView.as_view(), name="client_detail"),
     path("clients/<int:pk>/delete/", NewsletterRecipientDeleteView.as_view(), name="client_delete"),
+
     path("messages/", MessageListView.as_view(), name="messages_list"),
     path("messages/new/", MessageCreateView.as_view(), name="message_create"),
     path("messages/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
     path("messages/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
-    path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete")
+    path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
+
+    path("newsletters/new/", NewsletterCreateView.as_view(), name="newsletter_create"),
+    path("newsletters/", NewsletterListView.as_view(), name="newsletters_list"),
+    path("newsletters/<int:pk>/", NewsletterDetailView.as_view(), name="newsletter_detail"),
+    path("newsletters/<int:pk>/update/", NewsletterUpdateView.as_view(), name="newsletter_update"),
+    path("newsletters/<int:pk>/delete/", NewsletterDeleteView.as_view(), name="newsletter_delete"),
 ]
