5c4b1fc02c0543b3ca2f9d160ced8843b580530e
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-08 08:11:56 +0200

    Urls for service views registered

--- newsletters/urls.py
+++ newsletters/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from newsletters.apps import NewslettersConfig
+from newsletters.services import (NewsletterConfirmStart, NewsletterStartFailed, NewsletterStartSuccess,
+                                  StartNewsletterView)
 from newsletters.views import (MessageCreateView, MessageDeleteView, MessageDetailView, MessageListView,
-                               MessageUpdateView, NewsletterRecipientCreateView, NewsletterRecipientDeleteView,
+                               MessageUpdateView, NewsletterCreateView, NewsletterDeleteView, NewsletterDetailView,
+                               NewsletterListView, NewsletterRecipientCreateView, NewsletterRecipientDeleteView,
                                NewsletterRecipientDetailView, NewsletterRecipientListView,
-                               NewsletterRecipientUpdateView, NewsletterCreateView, NewsletterListView,
-                               NewsletterDetailView, NewsletterUpdateView, NewsletterDeleteView)
+                               NewsletterRecipientUpdateView, NewsletterUpdateView)
 
 app_name = NewslettersConfig.name
 
@@ -15,16 +17,18 @@ urlpatterns = [
     path("clients/<int:pk>/update/", NewsletterRecipientUpdateView.as_view(), name="client_update"),
     path("clients/<int:pk>/", NewsletterRecipientDetailView.as_view(), name="client_detail"),
     path("clients/<int:pk>/delete/", NewsletterRecipientDeleteView.as_view(), name="client_delete"),
-
     path("messages/", MessageListView.as_view(), name="messages_list"),
     path("messages/new/", MessageCreateView.as_view(), name="message_create"),
     path("messages/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
     path("messages/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
     path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
-
     path("newsletters/new/", NewsletterCreateView.as_view(), name="newsletter_create"),
     path("newsletters/", NewsletterListView.as_view(), name="newsletters_list"),
     path("newsletters/<int:pk>/", NewsletterDetailView.as_view(), name="newsletter_detail"),
     path("newsletters/<int:pk>/update/", NewsletterUpdateView.as_view(), name="newsletter_update"),
     path("newsletters/<int:pk>/delete/", NewsletterDeleteView.as_view(), name="newsletter_delete"),
+    path("newsletters/<int:pk>/start/", StartNewsletterView.as_view(), name="newsletter_start"),
+    path("newsletters/<int:pk>/start/confirm/", NewsletterConfirmStart.as_view(), name="newsletter_confirm"),
+    path("newsletters/<int:pk>/start/success/", NewsletterStartSuccess.as_view(), name="newsletter_success"),
+    path("newsletters/<int:pk>/start/failed/", NewsletterStartFailed.as_view(), name="newsletter_failed"),
 ]
