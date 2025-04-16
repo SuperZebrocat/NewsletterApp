adc6a337bf303067e9d6b3bb42286def39dafe71
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-16 12:09:11 +0200

    Login page defined as app entry point

--- users/urls.py
+++ users/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("login/password_reset/done/", UserPasswordResetDoneView.as_view(), name="password_reset_done"),
     path("login/reset/<uidb64>/<token>/", UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("login/password_reset/complete/", UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
-    path("login/", CustomLoginView.as_view(), name="login"),
+    path("", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="users:login"), name="logout"),
     path("profile/<str:email>/", UserDetailView.as_view(), name="profile_detail"),
     path("profile/<str:email>/update/", UserUpdateView.as_view(), name="profile_update"),
