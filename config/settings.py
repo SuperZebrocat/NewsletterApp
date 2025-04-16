104527d5e683241f84be72826abcb7623537539d
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-16 12:04:18 +0200

    Media root fixed

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR)
+MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
