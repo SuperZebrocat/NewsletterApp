1fac4abe0ff299ca41e461b3bfadef4b644bc4ee
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-16 12:07:10 +0200

    Small fixes

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
     avatar = ImageField(
-        upload_to="avatars/", verbose_name="Аватар", blank=True, null=True, default="media/avatars/default_avatar.jpg"
+        upload_to="avatars/", verbose_name="Аватар", blank=True, null=True, default="avatars/default_avatar.jpg"
     )
     phone_number = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True)
     country = models.CharField(max_length=150, verbose_name="Страна", blank=True, null=True)
